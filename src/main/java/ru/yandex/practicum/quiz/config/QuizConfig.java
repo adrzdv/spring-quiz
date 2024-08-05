edf13ce32426d3ae7219af5c0005701fedffea70
adrzdv <anton.drozdov2015@yandex.ru>
2024-08-05 22:14:35 +0300

    init commit

--- src/main/java/ru/yandex/practicum/quiz/config/QuizConfig.java
+++ src/main/java/ru/yandex/practicum/quiz/config/QuizConfig.java
@@ -3,9 +3,7 @@ package ru.yandex.practicum.quiz.config;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.quiz.model.Question;
 
 import java.util.List;
